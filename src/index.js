1edb287213375e5493178a94e417d7fecfcfe2a5
Anthony Lyons <anthony@thelyons.me>
2021-07-13 12:46:09 -0400

    Added request params for query

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ const express = require("express");
 const server = express();
 const axios = require("axios");
 const cors = require("cors");
-// server.get("*", (request, response) => {
-//   response.send("Hello, World!");
-//   response.end();
-// });
 
 server.use(express.json());
 
 server.use(cors());
 
 server.get("/weather", async function (req, res) {
+  const query = req.query.query;
   const weather = await axios.get(
-    "http://api.weatherstack.com/current?access_key=5a4893407f6520d0353810a4e0bf0ad6&query=Atlanta&units=f"
+    `http://api.weatherstack.com/current?access_key=5a4893407f6520d0353810a4e0bf0ad6&query=${query}&units=f`
   );
   console.log(weather);
   res.send(weather.data);
